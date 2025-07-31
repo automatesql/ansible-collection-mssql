b3b3af23023286f9ac4c234d305d5bd31a7c3bb8
Luke Campbell <Luke.Campbell@automatesql.com>
2025-07-31 17:37:04 -0500

    cleanup

--- .ansible/collections/ansible_collections/automatesql/mssql/roles/sql_ag_patch/files/get_sqlversion.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-      param(
-        [Parameter(Mandatory=$true)]
-        [string]$ServerInstance
-      )
-      import-module SqlServer
-      (Get-SqlInstance -ServerInstance "$ServerInstance" -ErrorAction Stop ).Version.ToString()
-
-      $Ansible.Changed = $false
--- .ansible/collections/ansible_collections/automatesql/mssql/roles/sql_install/files/sql_scripts/Get-NumErrorLogs.sql
+++
@@ -1,9 +0,0 @@
-declare @Results table (Value varchar(300), Data int)
-declare @data int
-insert into @Results
-EXEC xp_instance_regread N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'NumErrorLogs'
-
-select @data = data from @Results
-
-print isnull(@data,0)
-
